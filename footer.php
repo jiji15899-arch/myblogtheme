a43b3515bc755c68c88b87272f69c3d9a947a019
jiji15899-arch <jiji15899@gmail.com>
2026-01-19 21:42:31 +0900

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,23 @@
+</div><!-- .site-content -->
+</div><!-- .site -->
+
+<div class="site-footer grid-container">
+    <footer class="site-info" aria-label="사이트" itemtype="https://schema.org/WPFooter" itemscope>
+        <div class="inside-site-info grid-container">
+            <div class="copyright-bar">
+                &copy; <?php echo date('Y'); ?> Built with <a href="https://generatepress.com/?ref=811" target="_blank">GeneratePress</a>
+            </div>
+        </div>
+    </footer>
+</div>
+
+<a title="스크롤해서 다시 올라가기" aria-label="스크롤해서 다시 올라가기" rel="nofollow" href="#" class="generate-back-to-top" data-scroll-speed="400" data-start-scroll="300" role="button">
+    <span class="gp-icon icon-arrow-up">
+        <?php echo blog7_get_svg('arrow-up'); ?>
+    </span>
+</a>
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
